b7514445c2b9d4cdd4c66450e523df73c1da9a6e
yearlongmason <mdudelee@gmail.com>
2025-04-15 09:36:18 -0400

    Updated file io

--- stats.js
+++ stats.js
@@ -1,17 +1,20 @@
 let gameTablePath = "./Scrapers/data/gameTable.json";
+let gameData = null;
+
+window.onload = async (event) => {
+    // Call the function to load the data
+    await fetchJson();
+    console.log(gameTable)
+};
 
 async function fetchJson() {
-    try {
-      const response = await fetch(gameTablePath);
-      if (!response.ok) throw new Error(`HTTP error! Status: ${response.status}`);
-      const data = await response.json();
-      return data;
-    } catch (err) {
-      console.error('Fetch error:', err);
-      return null;
-    }
+  try {
+    const response = await fetch(gameTablePath);
+    if (!response.ok) throw new Error(`HTTP error! Status: ${response.status}`);
+    const data = await response.json();
+    gameTable = data; // Save the JSON to the variable
+    console.log('Game data loaded!');
+  } catch (err) {
+    console.error('Fetch error:', err);
   }
-  
-  fetchJson().then(data => {
-    console.log(data);
-  });
+}
