e310a64d1da8c35bbd8a3996c313c4e49db40ad9
yearlongmason <mdudelee@gmail.com>
2025-04-22 10:59:38 -0400

    Added visualizations to stats page

--- stats.js
+++ stats.js
@@ -5,8 +5,11 @@ window.onload = async (event) => {
     // Call the function to load the data
     await fetchJson();
     updateHeader();
-    console.log(gameTable)
-    console.log(getTotalTimePlayed(gameTable))
+    renderMostPlayedGames();
+    renderClosestTo100PercentChart();
+    renderPlaytimeVSPercentage();
+    renderCompletedVSNotCompleted();
+    //console.log(gameTable)
 };
 
 async function fetchJson() {
@@ -32,7 +35,6 @@ function getTotalTimePlayed(gameTable) {
 }
 
 function getTotalScore(gameTable, scoreType) {
-    console.log(gameTable[0]["GS"].split(" / ")[0])
     let totalGS = gameTable.reduce((accumulator, currentValue) => accumulator + Number(currentValue[scoreType].split(" / ")[0].replace(/,/g, '')), 0);
     return totalGS.toLocaleString()
 }
@@ -56,4 +58,225 @@ function updateHeader() {
     document.getElementById("totalGamerscore").innerText = getTotalScore(gameTable, "GS")
     document.getElementById("totalTrueAchievementsScore").innerText = getTotalScore(gameTable, "TA")
     document.getElementById("mostPlayedGame").innerText = getMostPlayedGame(gameTable)
+}
+
+function renderMostPlayedGames(){
+    // Get top 10 most played games
+    sortedGames = gameTable.sort((a, b) => a["Minutes played"] - b["Minutes played"])
+    if(sortedGames.length > 10) {
+        sortedGames = sortedGames.slice(sortedGames.length - 10)
+    }
+
+    const chartCanvas = document.getElementById('MostPlayedGameChart').getContext('2d');
+    const chart = new Chart(chartCanvas, {
+        type: "bar",
+        data: {
+            labels: sortedGames.map((game) => game.Title), // x axis labels
+            datasets: [{
+                label: 'Minutes Played',
+                data: sortedGames.map((game) => game["Minutes played"]), // Y-axis data
+                backgroundColor: '#2ecc71', // Bar color
+                borderColor: '#2ecc71', // Border color
+                borderWidth: 1
+            }]
+        },
+        options: {
+            responsive: true,
+            maintainAspectRatio: false,
+            plugins: {
+                legend: {
+                    display: false
+                },
+                tooltip: {
+                    callbacks: {
+                        label: function(context) {
+                            const game = sortedGames[context.dataIndex]
+                            return `Time Played: ${game["Time played"]}`;
+                        }
+                    }
+                }
+            },
+            scales: {
+                y: {
+                    beginAtZero: true,
+                    title: {
+                        display: true,
+                        text: 'Minutes Played',
+                        font: {
+                            size: 14
+                        }
+                    }
+                },
+                x: {
+                    title: {
+                        display: true,
+                        text: 'Game Title',
+                        font: {
+                            size: 14
+                        }
+                    }
+                }
+            }
+        }
+    })
+}
+
+function renderClosestTo100PercentChart(){
+    // Get top 10 most played games
+    sortedGames = gameTable.sort((a, b) => a["%age"] - b["%age"])
+    sortedGames = sortedGames.filter((game) => game["%age"] != 100)
+    if(sortedGames.length > 10) {
+        sortedGames = sortedGames.slice(sortedGames.length - 10)
+    }
+
+    let lowestPercentage = Math.min(...sortedGames.map((game) => game["%age"]))
+
+    const chartCanvas = document.getElementById('ClosestTo100PercentChart').getContext('2d');
+    const chart = new Chart(chartCanvas, {
+        type: "bar",
+        data: {
+            labels: sortedGames.map((game) => game.Title), // x axis labels
+            datasets: [{
+                label: '% Completed',
+                data: sortedGames.map((game) => game["%age"]), // Y-axis data
+                backgroundColor: '#2ecc71', // Bar color
+                borderColor: '#2ecc71', // Border color
+                borderWidth: 1
+            }]
+        },
+        options: {
+            responsive: true,
+            maintainAspectRatio: false,
+            plugins: {
+                legend: {
+                    display: false
+                },
+                tooltip: {
+                    callbacks: {
+                        label: function(context) {
+                            const game = sortedGames[context.dataIndex]
+                            return `${game["%age"]}% Completed`;
+                        }
+                    }
+                }
+            },
+            scales: {
+                y: {
+                    min: Math.max(lowestPercentage - 20, 0),
+                    max: 100,
+                    title: {
+                        display: true,
+                        text: 'Minutes Played',
+                        font: {
+                            size: 14
+                        }
+                    }
+                },
+                x: {
+                    title: {
+                        display: true,
+                        text: 'Game Title',
+                        font: {
+                            size: 14
+                        }
+                    }
+                }
+            }
+        }
+    })
+}
+
+function renderPlaytimeVSPercentage(){
+    //console.log(gameTable)
+    let timeVSPercentage = gameTable.map(game => { return {x: game["%age"], 
+        y: game["Minutes played"],
+    title: game["Title"],
+    timePlayed: game["Time played"]} })
+
+    const data = {
+        datasets: [{
+            label: 'Test Dataset',
+            data: timeVSPercentage,
+            backgroundColor: '#2ecc71'
+        }]
+    };
+
+    const config = {
+        type: 'scatter',
+        data: data,
+        options: {
+            responsive: true,
+            plugins: {
+                legend: {
+                    display: false
+                },
+                tooltip: {
+                    callbacks: {
+                        label: function(context) {
+                            const game = timeVSPercentage[context.dataIndex]
+                            return `${game.title} | ${game.x}% Completed | ${game.timePlayed}`;
+                        }
+                    }
+                }
+            },
+            scales: {
+                x: {
+                    type: 'linear',
+                    position: 'bottom',
+                    title: {
+                        display: true,
+                        text: 'Percent completed',
+                        font: {
+                            size: 14
+                        }
+                    }
+                },
+                y: {
+                    title: {
+                        display: true,
+                        text: 'Minutes Played',
+                        font: {
+                            size: 14
+                        }
+                        
+                    },
+                    //type: "logarithmic"
+                }
+            }
+        }
+    };
+
+    const chartCanvas = document.getElementById('PlaytimeVSPercentage').getContext('2d');
+    new Chart(chartCanvas, config);
+}
+
+function renderCompletedVSNotCompleted(){
+    let completedGames = gameTable.filter((game) => game["%age"] == 100).length;
+    let inProgressGames = gameTable.filter((game) => game["%age"] != 100).length;
+
+    const data = {
+        labels: ['Completed', 'In Progress'],
+        datasets: [{
+            label: 'Game Progress',
+            data: [completedGames, inProgressGames],
+            backgroundColor: ['#2ecc71', '#2e2e2e'], // green and dark grey
+            borderColor: ['#2ecc71', '#2e2e2e'],
+            borderWidth: 1
+        }]
+    };
+
+    const config = {
+        type: 'pie',
+        data: data,
+        options: {
+            responsive: true,
+            plugins: {
+                legend: {
+                    position: 'bottom'
+                }
+            }
+        }
+    };
+
+    new Chart(document.getElementById('CompletedVSNotCompleted').getContext('2d'), config);
 }
